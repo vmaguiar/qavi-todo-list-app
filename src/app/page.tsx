fc152873fc7df4c21275f4ddb6488f47602ee1ef
vmaguiar <vmaguiar@hotmail.com>
2023-05-30 17:18:33 -0300

    feat: add states to catch inputs text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,42 @@
 'use client'
 
-import { useState } from "react"
+import { ChangeEvent, useState } from "react"
 
 import { Todo } from "./Todo"
 
+interface ItodoCard {
+  title: string,
+  todoList: string[],
+  todoDoneList: string[]
+};
 
 export default function Home() {
-  const [todoList, setTodoList] = useState([])
+  const [todoCard, setTodoCard] = useState<ItodoCard>({
+    title: '',
+    todoList: [],
+    todoDoneList: []
+  });
+
+  const handleOnChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const {value, name} = event.target;
+    // console.log(name);
+
+    if(name === 'title') {
+      setTodoCard((oldTodoCard) => {
+        const newTodoList = {...oldTodoCard, title: value}
+        console.log(`entrou no title => ${newTodoList}`);
+        return newTodoList;
+      })
+    }
+
+    else if(name === 'singletodoText') {
+      console.log('entrou no todo list')
+      setTodoCard((oldTodoCard) => {
+        const newTodoCard = {...oldTodoCard, todoList: [value]}
+        return newTodoCard
+      })
+    }
+  };
 
   return (
     <div className="min-h-screen">
@@ -15,35 +45,35 @@ export default function Home() {
         <p>Made by: @vmaguiar</p>
       </header>
 
-    <main className="flex flex-col items-center justify-between my-16">
-      <div className="flex flex-col items-center w-2/4 rounded-md border-solid border-2 border-gray-900 p-6">
-        <p>add title</p>
-        <input type="text" className="w-2/3"/>
-        <p>add tasks</p>
-        <input type="text" className="w-full"/>
-      </div>
+      <main className="flex flex-col items-center justify-between my-16">
+        <div className="flex flex-col items-center w-2/4 rounded-md border-solid border-2 border-gray-700 p-6">
+          <p>add title</p>
+          <input type="text" name="title" className="w-2/3" onChange={(event) => { handleOnChange(event)}}/>
+          <p>add todo</p>
+          <input type="text" name="singletodoText" className="w-full" onChange={(event) => { handleOnChange(event)}}/>
+        </div>
       
-      {/* Container with all to do cards */}
-      <div className="grid grid-flow-col auto-cols-fr gap-4 my-8">
-        <Todo />
-        <Todo />
-        <Todo />
-        <Todo />
-        <Todo />
-        <Todo />
-        <Todo />
-        <Todo />
-        <Todo />
-        <Todo />
-        <Todo />
-        <Todo />
-        <Todo />
-        <Todo />
-        <Todo />
-        <Todo />
-        <Todo />
-      </div>
-    </main>
+        {/* Container with all to do cards */}
+        <div className="grid grid-flow-col auto-cols-fr gap-4 my-8">
+          <Todo />
+          <Todo />
+          <Todo />
+          <Todo />
+          <Todo />
+          <Todo />
+          <Todo />
+          <Todo />
+          <Todo />
+          <Todo />
+          <Todo />
+          <Todo />
+          <Todo />
+          <Todo />
+          <Todo />
+          <Todo />
+          <Todo />
+        </div>
+      </main>
     </ div>
   )
-}
+};
