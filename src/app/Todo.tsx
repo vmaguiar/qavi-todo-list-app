5dfd49d34725c13a1b28e0042ee44b05c273f06d
vmaguiar <vmaguiar@hotmail.com>
2023-05-31 12:03:50 -0300

    feat: create a component for todo

--- src/app/Todo.tsx
+++ src/app/Todo.tsx
@@ -1,9 +1,29 @@
+interface TodoProps {
+  todoText: string,
+  index: number
+}
 
+export function Todo(props: TodoProps) {
+  return (
+    <div className="flex flex-row justify-between" key={props.index}>
+      <div>
+        <input type="checkbox" name={props.todoText} id={props.todoText} />
+        <label htmlFor={props.todoText}> {props.todoText} </label>
+      </div>
 
-export function Todo() {
-    return(
-        <div>
-            "To do" cards
-        </div>
-    )
+      <div className="ml-4">
+        <button
+          className="mr-2"
+        >
+          Edit
+        </button>
+        <button
+          className="ml-2"
+        // onClick={(event) => { deleteTodoItem(event) }}
+        >
+          Del
+        </button>
+      </div>
+    </div>
+  )
 }
