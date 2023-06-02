e01256973469504c01c90398a2cd7e6b81fc7db8
vmaguiar <vmaguiar@hotmail.com>
2023-06-02 09:55:09 -0300

    refactor: changes component name

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,6 +1,26 @@
-import { Todo } from "./Todo"
+import { useEffect, useState } from "react"
 
-export function Card() {
+import { Todo } from "./Todo"
+import { ItodoCard } from "./CreateCardForm"
+
+export function Cards() {
+  const [cards, setCards] = useState<ItodoCard>({
+    title: '',
+    todoList: [],
+    todoDoneList: []
+  })
+
+  useEffect(() => {
+
+  }, [])
+
+  const getDataFromStorage = () => {
+    const storedCard = localStorage.getItem('cards')
+    if (storedCard) {
+      setCards(JSON.parse(storedCard))
+    }
+  }
+
   return (
     <div className="flex flex-col w-[250px] relative mx-auto mt-8 rounded-lg bg-white p-2 shadow-[0_1px_7px_0px_rgba(0,0,0,0.5)]">
       <h1>My Tasks Title</h1>
@@ -15,17 +35,10 @@ export function Card() {
 
       <input type="checkbox" name="4TaskName" id="4TaskId" value="4TaskValue" />
       <label htmlFor="4TaskId">4th Task</label> */}
-      <Todo todoText={'primeiro todo'} index={1} /> <hr />
-      <Todo todoText={'segundo todo'} index={2} />
-      <Todo todoText={'terceiro todo'} index={3} />
-      <Todo todoText={'quarto todo'} index={4} />
-      <Todo todoText={'quinto todo'} index={5} />
-      <Todo todoText={'sexto todo'} index={6} />
-      <Todo todoText={'setimo todo'} index={7} />
-      <Todo todoText={'oitavo todo'} index={8} />
-      <Todo todoText={'nono todo'} index={9} />
-      <Todo todoText={'decimoasdasadsdasdasdasdasd asasdasdprimeiro todo'} index={10} key={10} />
-      <Todo todoText={'decimo segundo todo'} index={11} key={11} />
+
+      {/* <Todo todoText={'primeiro todo'} index={1} /> <hr /> */}
+
+
     </div>
   )
 }
