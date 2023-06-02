06ccdf7ac152d898c58788ae3d13d73cb39e54e9
vmaguiar <vmaguiar@hotmail.com>
2023-06-02 18:52:22 -0300

    feat: create delete/edit to cards (incomplete)

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -7,6 +7,15 @@ import { CardsContext } from "@/utils/CardsContext"
 export function Cards() {
   const { cardsList, setCardsList } = useContext(CardsContext)
 
+  const deleteCard = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    console.log('Deletou Card')
+    const { value } = event.currentTarget
+  }
+
+  const editCard = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    console.log('entrou no Edit Mode')
+  }
+
   return (
     <>
       {
@@ -14,7 +23,21 @@ export function Cards() {
           <div className="flex flex-col w-[250px] relative mx-auto mt-8 rounded-lg bg-white p-2 shadow-[0_1px_7px_0px_rgba(0,0,0,0.5)]"
             key={index}
           >
-            <h1>{item.title}</h1>
+            <div className="flex flex-row justify-between">
+              <h1>{item.title}</h1>
+
+              <div>
+                <button onClick={(event) => { editCard(event) }}>
+                  📝
+                </button>
+
+                <button onClick={(event) => {
+                  deleteCard(event)
+                }}>
+                  ✖️
+                </button>
+              </div>
+            </div>
             {
               item.todoList.map((todoItem, todoIndex) => (
                 <div key={todoIndex}>
