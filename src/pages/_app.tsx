90b72287f734bedbaf813d6a36d580ad52804f47
vmaguiar <vmaguiar@hotmail.com>
2023-06-05 17:47:20 -0300

    feat: add a toaster provider to use

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,19 @@
 import { type AppType } from "next/app";
+
 import { api } from "~/utils/api";
+
 import "~/styles/globals.css";
 
+import { Toaster } from "react-hot-toast";
+
+
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Toaster />
+    </>
+  );
 };
 
 export default api.withTRPC(MyApp);
